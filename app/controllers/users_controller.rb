7e976d03bf1e9247dc9f80bdea965bed56c03a9d
nky4593 <nky451993@gmail.com>
2019-06-04 09:46:06 +0700

    Chapter10: Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :select_user, only: :show
+  before_action :load_user, only: :show
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     return if @user
@@ -24,6 +26,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "updated"
+      redirect_to @user
+    else
+      flash.now[:danger] = t "update_fail"
+      render :edit
+    end
+  end
+
   private
 
   def user_params
@@ -31,7 +45,25 @@ class UsersController < ApplicationController
       :password_confirmation
   end
 
-  def select_user
+  def load_user
     @user = User.find_by id: params[:id]
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "plslogin"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    begin
+      redirect_to root_url unless @user.current_user? current_user
+    rescue StandardError
+      flash[:danger] = t "notfound"
+      redirect_to root_url
+    end
   end
 end
